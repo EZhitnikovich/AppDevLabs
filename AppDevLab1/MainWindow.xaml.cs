564935004ffe05a8904fe742197c4f2c373ca509
Warden-labs <ezhitnikovich@gmail.com>
2020-10-18 00:24:33 +0300

    First lab

---
+++ AppDevLab1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppDevLab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void executeButton_Click(object sender, RoutedEventArgs e)
+        {
+            double X = Manager.ConvertToDouble(xInput.Text);
+            double Y = Manager.ConvertToDouble(yInput.Text);
+            double Z = Manager.ConvertToDouble(zInput.Text);
+
+            string Answer = $"X = {X}\r\n" +
+                            $"Y = {Y}\r\n" +
+                            $"Z = {Z}\r\n" +
+                            $"Результат U = {Manager.Calculate(X, Y, Z)}\r\n";
+
+            Output.Text += Answer;
+        }
+    }
+}
---
+++ AppDevLab1/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Math;
+
+namespace AppDevLab1
+{
+    class Manager
+    {
+        public static double Calculate(double X, double Y, double Z)
+        {
+            return Log(Pow(Y, -Sqrt(Abs(X)))) *
+                   (X - Y / 2) + Pow(Sin(Atan(Z)), 2);
+        }
+
+        public static double ConvertToDouble(string textValue)
+        {
+            try
+            {
+                double doubleValue = Convert.ToDouble(textValue);
+                return doubleValue;
+            }
+            catch (System.FormatException)
+            {
+                throw new FormatException();
+            }
+        }
+    }
+}
