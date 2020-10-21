21bd39d2cf9d9f2dde2c4f1ce23a817708f9a6bb
EZhitnikovich <ezhitnikovich@gmail.com>
2020-10-21 23:23:44 +0300

    add Lab5

---
+++ AppDevLab5/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppDevLab5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void execute_Click(object sender, RoutedEventArgs e)
+        {
+            string lineText = comboBox.SelectedItem.ToString();
+
+            ShowLineInfo(lineText);
+        }
+
+        private void textBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text != "0" && e.Text != "1" && e.Text != " ")
+            {
+                e.Handled = true;
+            }
+
+            if (textBox.Text != "" && e.Text == "\r")
+            {
+                listBox.Items.Insert(listBox.Items.Count, textBox.Text);
+                comboBox.Items.Insert(comboBox.Items.Count, textBox.Text);
+                textBox.Clear();
+            }
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string lineText = listBox.SelectedItem.ToString();
+
+            ShowLineInfo(lineText);
+        }
+
+        private void ShowLineInfo(string lineText)
+        {
+            List<string> array = Manager.GetOddGroups(lineText);
+
+            string answer = null;
+
+            foreach (string s in array)
+            {
+                answer += $"В {s} - {Manager.CalculateUnits(s)} по 1\r\n";
+            }
+
+            MessageBox.Show(answer);
+        }
+    }
+}
---
+++ AppDevLab5/Manager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AppDevLab5
+{
+    class Manager
+    {
+        public static List<string> GetOddGroups(string originalText)
+        {
+            List<string> finalArray = new List<string>() { };
+
+            finalArray = originalText.Split(' ').ToList();
+
+            foreach (string s in finalArray.ToArray())
+            {
+                if (s.Length % 2 != 1)
+                {
+                    finalArray.Remove(s);
+                }
+            }
+
+            return finalArray;
+        }
+
+        public static int CalculateUnits(string element)
+        {
+            //int quantity = element.Count(x => x == '1');
+
+            int quantity = 0;
+
+            for(int i = 0; i < element.Length; i++)
+            {
+                if(element[i] == '1')
+                {
+                    quantity++;
+                }
+            }
+
+            return quantity;
+        }
+    }
+}
