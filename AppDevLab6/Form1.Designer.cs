106d44319856e98db0ba8bbde64e32bdc76db805
Eugenii <72632604+EZhitnikovich@users.noreply.github.com>
2020-10-31 15:00:31 +0300

    add Lab6

---
+++ AppDevLab6/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace LAB6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxData = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxCCode = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxCTime = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxTax = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxNA = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxNCity = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonFOpen = new System.Windows.Forms.Button();
+            this.buttonExecute = new System.Windows.Forms.Button();
+            this.buttonFSave = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBoxData
+            // 
+            this.textBoxData.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxData.Location = new System.Drawing.Point(471, 9);
+            this.textBoxData.Name = "textBoxData";
+            this.textBoxData.Size = new System.Drawing.Size(100, 23);
+            this.textBoxData.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(352, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Дата разговора";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(353, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Код города";
+            // 
+            // textBoxCCode
+            // 
+            this.textBoxCCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxCCode.Location = new System.Drawing.Point(471, 38);
+            this.textBoxCCode.Name = "textBoxCCode";
+            this.textBoxCCode.Size = new System.Drawing.Size(100, 23);
+            this.textBoxCCode.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(27, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Название города";
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxCity.Location = new System.Drawing.Point(200, 99);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(100, 23);
+            this.textBoxCity.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(27, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Время разговора (мин)";
+            // 
+            // textBoxCTime
+            // 
+            this.textBoxCTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxCTime.Location = new System.Drawing.Point(200, 12);
+            this.textBoxCTime.Name = "textBoxCTime";
+            this.textBoxCTime.Size = new System.Drawing.Size(100, 23);
+            this.textBoxCTime.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(352, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Тариф (мин/руб)";
+            // 
+            // textBoxTax
+            // 
+            this.textBoxTax.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxTax.Location = new System.Drawing.Point(471, 67);
+            this.textBoxTax.Name = "textBoxTax";
+            this.textBoxTax.Size = new System.Drawing.Size(100, 23);
+            this.textBoxTax.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(27, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(147, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Ном. тел. (абонента)";
+            // 
+            // textBoxNA
+            // 
+            this.textBoxNA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxNA.Location = new System.Drawing.Point(200, 70);
+            this.textBoxNA.Name = "textBoxNA";
+            this.textBoxNA.Size = new System.Drawing.Size(100, 23);
+            this.textBoxNA.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(27, 44);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(140, 17);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Ном. тел. (в городе)";
+            // 
+            // textBoxNCity
+            // 
+            this.textBoxNCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxNCity.Location = new System.Drawing.Point(200, 41);
+            this.textBoxNCity.Name = "textBoxNCity";
+            this.textBoxNCity.Size = new System.Drawing.Size(100, 23);
+            this.textBoxNCity.TabIndex = 14;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(355, 96);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(216, 23);
+            this.buttonAdd.TabIndex = 16;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonFOpen
+            // 
+            this.buttonFOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFOpen.Location = new System.Drawing.Point(296, 376);
+            this.buttonFOpen.Name = "buttonFOpen";
+            this.buttonFOpen.Size = new System.Drawing.Size(136, 23);
+            this.buttonFOpen.TabIndex = 17;
+            this.buttonFOpen.Text = "Открыть файл";
+            this.buttonFOpen.UseVisualStyleBackColor = true;
+            this.buttonFOpen.Click += new System.EventHandler(this.buttonFOpen_Click);
+            // 
+            // buttonExecute
+            // 
+            this.buttonExecute.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonExecute.Location = new System.Drawing.Point(13, 376);
+            this.buttonExecute.Name = "buttonExecute";
+            this.buttonExecute.Size = new System.Drawing.Size(135, 23);
+            this.buttonExecute.TabIndex = 18;
+            this.buttonExecute.Text = "Выполнить";
+            this.buttonExecute.UseVisualStyleBackColor = true;
+            this.buttonExecute.Click += new System.EventHandler(this.buttonExecute_Click);
+            // 
+            // buttonFSave
+            // 
+            this.buttonFSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFSave.Location = new System.Drawing.Point(154, 376);
+            this.buttonFSave.Name = "buttonFSave";
+            this.buttonFSave.Size = new System.Drawing.Size(136, 23);
+            this.buttonFSave.TabIndex = 19;
+            this.buttonFSave.Text = "Сохранить в файл";
+            this.buttonFSave.UseVisualStyleBackColor = true;
+            this.buttonFSave.Click += new System.EventHandler(this.buttonFSave_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(13, 128);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox1.Size = new System.Drawing.Size(558, 242);
+            this.richTextBox1.TabIndex = 20;
+            this.richTextBox1.Text = "";
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClose.Location = new System.Drawing.Point(437, 376);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(134, 23);
+            this.buttonClose.TabIndex = 21;
+            this.buttonClose.Text = "Закрыть";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 411);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonFSave);
+            this.Controls.Add(this.buttonExecute);
+            this.Controls.Add(this.buttonFOpen);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxNCity);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxNA);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxTax);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxCTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxCCode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxData);
+            this.MaximumSize = new System.Drawing.Size(600, 450);
+            this.MinimumSize = new System.Drawing.Size(600, 450);
+            this.Name = "Form1";
+            this.Text = "Lab6";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxCCode;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxCTime;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxTax;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxNA;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxNCity;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonFOpen;
+        private System.Windows.Forms.Button buttonExecute;
+        private System.Windows.Forms.Button buttonFSave;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ AppDevLab6/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using static LAB6.Manager;
+
+namespace LAB6
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, List<Item>> items = new Dictionary<string, List<Item>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt";
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt";
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonFOpen_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            items = new Dictionary<string, List<Item>>();
+
+            string filename = openFileDialog1.FileName;
+
+            string fileText = File.ReadAllText(filename);
+
+            richTextBox1.Text = fileText;
+
+            MessageBox.Show("Файл открыт\n(для просмотра)");
+        }
+
+        private void buttonFSave_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            string filename = saveFileDialog1.FileName;
+
+            string str = null;
+
+            foreach (var element in items.Keys)
+            {
+                str += element + "\r\n";
+                foreach (var value in items[element])
+                {
+                    str += value.GetInfo();
+                }
+
+                str += "\n\r";
+            }
+
+            File.WriteAllText(filename, str);
+
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void buttonExecute_Click(object sender, EventArgs e)
+        {
+            string answer = null;
+
+            foreach (var el in items)
+            {
+                double allTime = 0;
+                double price = 0;
+                foreach (var value in el.Value)
+                {
+                    allTime += value.Time;
+                    price += value.Time * value.Tax;
+                }
+                answer += el.Key + ": среднее время " + allTime / el.Value.Count + ", цена " + price + " руб.";
+            }
+            richTextBox1.Text += answer;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            Item item = new Item
+            {
+                Time = ConvertToInt(textBoxCTime.Text),
+                NumberC = ConvertToInt(textBoxNCity.Text),
+                NumberA = ConvertToInt(textBoxNA.Text),
+                CityName = textBoxCity.Text,
+                Data = textBoxData.Text,
+                CityCode = ConvertToInt(textBoxCCode.Text),
+                Tax = ConvertToInt(textBoxTax.Text)
+            };
+
+            if (!items.ContainsKey(item.CityName.ToUpper()))
+                items.Add(item.CityName.ToUpper(), new List<Item> { item });
+            else
+                items[item.CityName.ToUpper()].Add(item);
+
+            string str = null;
+
+            foreach (var element in items.Keys)
+            {
+                str += element + "\r\n";
+                foreach (var value in items[element])
+                {
+                    str += value.GetInfo();
+                }
+
+                str += "\n\r";
+            }
+
+            richTextBox1.Text = str;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxCCode.Text = "7";
+            textBoxCity.Text = "Минск";
+            textBoxCTime.Text = "27";
+            textBoxData.Text = "17 январь";
+            textBoxNA.Text = "6652412";
+            textBoxNCity.Text = "2245212";
+            textBoxTax.Text = "2";
+        }
+    }
+}
